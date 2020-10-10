5d60445df0979a7345fe074c07db3ee656284918
Olga Murina <olgamurina92@gmail.com>
2020-10-10 14:41:33 +0300

    feat: add towelSort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (Array.isArray(matrix) && arguments.length) {
+    const mergerArray = [];
+
+    matrix.forEach((item, index) => {
+      index % 2 === 0 ? mergerArray.push(item) : mergerArray.push(item.reverse());
+    });
+
+    return mergerArray.flat(Infinity);
+  };
+  
   return [];
 }
